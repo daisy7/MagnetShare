6d621a3678c70fcd47dc8a545557ab427ba58770
daisy <75569325@qq.com>
2020-11-17 23:11:11 +0800

    change1

---
+++ nginx/html/xiunobbs/conf/conf.php
@@ -0,0 +1,110 @@
+<?php
+return array (
+  'db' => 
+  array (
+    'type' => 'pdo_mysql',
+    'mysql' => 
+    array (
+      'master' => 
+      array (
+        'host' => '47.111.16.6',
+        'user' => 'root',
+        'password' => 'mysql&7',
+        'name' => 'MagnetShare',
+        'tablepre' => 'bbs_',
+        'charset' => 'utf8mb4',
+        'engine' => 'innodb',
+      ),
+      'slaves' => 
+      array (
+      ),
+    ),
+    'pdo_mysql' => 
+    array (
+      'master' => 
+      array (
+        'host' => '47.111.16.6',
+        'user' => 'root',
+        'password' => 'mysql&7',
+        'name' => 'MagnetShare',
+        'tablepre' => 'bbs_',
+        'charset' => 'utf8mb4',
+        'engine' => 'innodb',
+      ),
+      'slaves' => 
+      array (
+      ),
+    ),
+  ),
+  'cache' => 
+  array (
+    'enable' => true,
+    'type' => 'mysql',
+    'memcached' => 
+    array (
+      'host' => 'localhost',
+      'port' => '11211',
+      'cachepre' => 'bbs_',
+    ),
+    'redis' => 
+    array (
+      'host' => 'localhost',
+      'port' => '6379',
+      'cachepre' => 'bbs_',
+    ),
+    'xcache' => 
+    array (
+      'cachepre' => 'bbs_',
+    ),
+    'yac' => 
+    array (
+      'cachepre' => 'bbs_',
+    ),
+    'apc' => 
+    array (
+      'cachepre' => 'bbs_',
+    ),
+    'mysql' => 
+    array (
+      'cachepre' => 'bbs_',
+    ),
+  ),
+  'tmp_path' => './tmp/',
+  'log_path' => './log/',
+  'view_url' => 'view/',
+  'upload_url' => 'upload/',
+  'upload_path' => './upload/',
+  'logo_mobile_url' => 'view/img/logo.png',
+  'logo_pc_url' => 'view/img/logo.png',
+  'logo_water_url' => 'view/img/water-small.png',
+  'sitename' => '',
+  'sitebrief' => '磁力分享MagnetShare是一个音乐/电影资源互相分享的平台,为了解决最新的音乐/电影方便搜索分享, 分享者互帮互助, 如果您有资源需求, 请注册账号并发布信息、详细描述需要的信息等, 我们会尽力帮您寻找',
+  'timezone' => 'Asia/Shanghai',
+  'lang' => 'zh-cn',
+  'runlevel' => 5,
+  'runlevel_reason' => 'The site is under maintenance, please visit later.',
+  'cookie_domain' => '',
+  'cookie_path' => '',
+  'auth_key' => 'T8UC3E57J489F9RC89W9QAA8KX85UMS7ZUEX4DBDZNHTZEYA9U88U9BPC6WH9GNR',
+  'pagesize' => 20,
+  'postlist_pagesize' => 100,
+  'cache_thread_list_pages' => 10,
+  'online_update_span' => 120,
+  'online_hold_time' => 3600,
+  'session_delay_update' => 0,
+  'upload_image_width' => 927,
+  'order_default' => 'lastpid',
+  'attach_dir_save_rule' => 'Ym',
+  'update_views_on' => 1,
+  'user_create_email_on' => 0,
+  'user_create_on' => 1,
+  'user_resetpw_on' => 1,
+  'admin_bind_ip' => 0,
+  'cdn_on' => 0,
+  'url_rewrite_on' => 0,
+  'disabled_plugin' => 0,
+  'version' => '4.0.6',
+  'static_version' => '?1.0',
+  'installed' => 1,
+);
+?>
